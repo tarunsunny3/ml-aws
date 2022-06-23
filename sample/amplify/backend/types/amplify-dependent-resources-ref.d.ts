fab89c5ca0eda58ef04cd07685695a4b39f44c2d
Tarun Chinthakindi <tarunsunny3@gmail.com>
2022-06-23 09:05:27 +0000

    first commit

---
+++ sample/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "sample8dde0460": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "identifyEntities72e19c15": {
+            "region": "string",
+            "celebrityDetectionEnabled": "string",
+            "maxEntities": "string"
+        }
+    }
+}
